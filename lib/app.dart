8cdd10c2485208f506ffbeb94f57b311e96ad481
Prashant-Chaudhary58 <prashanttharu441@gmail.com>
2025-12-10 22:17:45 +0545

    added theme data

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'package:mero_bazar/screens/auth/login_screen.dart';
 import 'package:mero_bazar/screens/auth/role_selection_screen.dart';
 import 'package:mero_bazar/screens/auth/signup_screen.dart';
 import 'package:mero_bazar/screens/home/home_screen.dart';
+import 'package:mero_bazar/theme/theme_data.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Mero Baazar",
       debugShowCheckedModeBanner: false,
+      theme: getApplicationTheme(),
       initialRoute: '/',
       routes: {
         '/': (context) => const RoleSelectionScreen(),
---
+++ lib/theme/theme_data.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    fontFamily: 'OpenSans Regular',
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: TextStyle(
+          fontSize: 18,
+          color: const Color.fromARGB(255, 234, 233, 229),
+          fontWeight: FontWeight.w500,
+          fontFamily: "OpenSans Regular"
+        ),
+        backgroundColor: Colors.green,
+        // backgroundColor: Color(0xFFbbdb44), (to add hex color)
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10)
+        )
+      )
+    ),
+  );
+}
