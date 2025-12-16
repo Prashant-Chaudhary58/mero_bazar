7dd9d1c98805d4c613708a59514393b1ef8b0930
Prashant-Chaudhary58 <prashanttharu441@gmail.com>
2025-12-16 09:30:06 +0545

    sprint 2 created

--- lib/screens/widgets/home/home_banner_widget.dart
+++ lib/screens/widgets/home/home_banner_widget.dart
@@ -7,40 +7,55 @@ class HomeBannerWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 120,
+      padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16),
         gradient: const LinearGradient(
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
           colors: [
             Color(0xFF6D8F4E),
             Color(0xFF88A76A),
           ],
         ),
       ),
-      padding: const EdgeInsets.all(16),
       child: Row(
         children: [
+          // Text section
           Expanded(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: const [
                 Text(
                   "Fresh. Fair. Direct",
                   style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
                 SizedBox(height: 6),
                 Text(
                   "बिचौलिया हटाएर, किसानको हातमा पैसा पुर्याऔं",
-                  style: TextStyle(color: Colors.white70, fontSize: 13),
+                  style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 13,
+                  ),
                 ),
               ],
             ),
           ),
-          const Icon(Icons.local_florist,
-              size: 40, color: Colors.white),
+
+          const SizedBox(width: 8),
+
+          // Image section
+          Image.asset(
+            'assets/images/flowerhome.png',
+            width: 90,
+            height: 90,
+            fit: BoxFit.contain,
+          ),
         ],
       ),
     );
